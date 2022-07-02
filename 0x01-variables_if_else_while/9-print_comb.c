a3747fba27458a93271068f4da829c62fc0f19e5
Dorcaz <kemiadebena@gmail.com>
2022-07-02 03:56:26 -0700

    My commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * *main-program that prints
+ * *combinations of single-digit numbers.
+ * *Description: writes 0-9 with comma and space
+ * *Return: zero on success
+ **/
+int main(void)
+{
+int num = 0;
+while (num <= 9)
+{
+	putchar(num + '0');
+	if (num < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	num++;									}
+putchar('\n');
+return (0);
+}
