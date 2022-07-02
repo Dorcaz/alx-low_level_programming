b7a0ee83abaae92a5e18e66bbfef7d6597d97a33
Dorcaz <kemiadebena@gmail.com>
2022-07-01 18:00:01 -0700

    My commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+{
+	if (c != 'q' || c != 'e')
+		putchar(c);
+}
+putchar('\n');
+return (0);
+}
