8e2d9570c99d07ea5a7e6dcc7c4da920336b157d
Dorcaz <kemiadebena@gmail.com>
2022-07-02 03:11:39 -0700

    My commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * *main- entry point
+ * *Description: Alphabets
+ * *Return: zero on success
+ **/
+int main(void)
+{
+char n = '0';
+while (n <= '9')
+{
+	putchar(n);
+	num++;									}
+putchar('\n');
+return (0);
+}
