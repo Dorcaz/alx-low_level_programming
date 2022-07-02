65f00806087b9c93b3559d0dbad29462c88902a1
Dorcaz <kemiadebena@gmail.com>
2022-07-02 04:29:49 -0700

    My commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,22 +6,22 @@
  **/
 int main(void)
 {
-	int i, j;
-	for (i = '0'; i <= '9'; i++)
+int i, j;
+for (i = '0'; i <= '9'; i++)
+{
+	for (j = '0'; j <= '9'; j++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		if ((i < j) & (j <= '9'))
 		{
-			if ((i < j) & (j <= '9'))
-			{
-				putchar(i);
-				putchar(j);
-			}
+			putchar(i);
+			putchar(j);
 			if ((j < '9') | (i < '8'))
 			{
 				putchar(',');
 				putchar(' ');
 			}
-		}									}
+		}
+	}
 putchar('\n');
 return (0);
 }
