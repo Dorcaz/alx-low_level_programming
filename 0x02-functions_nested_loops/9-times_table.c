11073014d20f0bb3297505619da3fa3c2b84d487
Dorcaz <kemiadebena@gmail.com>
2022-07-06 17:06:35 -0700

    My Commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,14 @@ void times_table(void)
 {
 	int a, b, product;
 
-	product = 0;
-
-
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
 			product = a * b
 				if (b != 0)
-				{');
+				{
+				_putchar(');
 				_putchar(' ');
 				}
 			if (product >= 10)
