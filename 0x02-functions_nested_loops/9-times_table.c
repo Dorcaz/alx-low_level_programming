b2cb6ca59ad2dd0c0c263040201cef40cd118e38
Dorcaz <kemiadebena@gmail.com>
2022-07-06 17:00:57 -0700

    My Commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,37 +1,40 @@
+
+
 #include "main.h"
-  /**
-   **times_table: prints times table up to 9
-   **Return: void
-   **/
+/**
+ * *times_table - prints times table up to 9
+ * *Return: void
+ **/
 void times_table(void)
 {
-	int a, b, result;
+	int a, b, product;
 
-	result = 0;
+	product = 0;
+
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			result = a * b;
-			if (b != 0)
-			{
-				_putchar(',');
+			product = a * b
+				if (b != 0)
+				{');
 				_putchar(' ');
+				}
+			if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+				}
+			else if ((product < 10) && (b != 0))
+			{
+				_putchar((product % 10) + '0');
 			}
-			if (result >= 10)
-				_putchar((result / 10) + '0');
-			_putchar((result % 10) + '0');
+			else
+			{
+				_putchar((product % 10) + '0');
+			}
 		}
-		else if ((result < 10) && (b != 0))
-			_putchar(' ');
-		_putchar((result % 10) + '0');
+		_putchar('\n');
 	}
-	else
-	{
-		_putchar((result % 10) + '0');
-	}
-}
-_putchar('\n');
-}
 }
